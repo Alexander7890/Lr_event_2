e1c217ddfeeb4a4523e39cd6644b4d145a2c832d
aleksandrkostin7890 <aleksandrkostin7890@gmail.com>
2024-05-07 21:31:27 +0300

    Add project files.

---
+++ Lr_event_4/CustomButton.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lr_event_4
+{
+    public class CustomButton : Button
+    {
+        // Переопределяем метод OnMouseClick
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            // Вызываем базовую реализацию метода
+            base.OnMouseClick(e);
+
+            // Получаем координаты клика мыши
+            int clickX = e.X;
+            int clickY = e.Y;
+
+            // Вычисляем расстояние до места клика
+            int dx = clickX - this.Left;
+            int dy = clickY - this.Top;
+
+            // Вычисляем шаг перемещения
+            int step = 5;
+
+            // Перемещаем кнопку к месту клика мыши с небольшими шагами
+            while (this.Left != clickX || this.Top != clickY)
+            {
+                if (this.Left < clickX)
+                    this.Left += Math.Min(step, clickX - this.Left);
+                else if (this.Left > clickX)
+                    this.Left -= Math.Min(step, this.Left - clickX);
+
+                if (this.Top < clickY)
+                    this.Top += Math.Min(step, clickY - this.Top);
+                else if (this.Top > clickY)
+                    this.Top -= Math.Min(step, this.Top - clickY);
+
+                // Обновляем форму
+                Application.DoEvents();
+                System.Threading.Thread.Sleep(10); // Для плавности движения
+            }
+        }
+    }
+}
---
+++ Lr_event_4/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lr_event_4
+{
+    public partial class Form1 : Form
+    {
+        private bool isUpMode = true; // Флаг, указывающий текущий режим движения
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Присваиваем обработчики событий кнопке и другим элементам управления
+            button2.Click += Control_Click;
+            textBox1.Click += Control_Click;
+            label1.Click += Control_Click;
+        }
+
+        private void Control_Click(object sender, EventArgs e)
+        {
+            // Вызываем метод, который двигает элементы вверх или вниз в зависимости от текущего режима
+            if (isUpMode)
+            {
+                MoveControlUp(sender as Control);
+            }
+            else
+            {
+                MoveControlDown(sender as Control);
+            }
+        }
+
+        private void ModeButton_Click(object sender, EventArgs e)
+        {
+            // Переключаем режим движения при нажатии на кнопку режима
+            isUpMode = !isUpMode;
+        }
+
+        private void MoveControlUp(Control control)
+        {
+            // Проверяем, что элемент не первый кнопка и еще не в самом верхнем положении формы
+            if (control != button1 && control.Top > 0)
+            {
+                int newTop = Math.Max(0, control.Top - 10); // Уменьшаем значение Top на 10 пикселей, но не ниже 0
+                control.Top = newTop;
+            }
+        }
+
+        private void MoveControlDown(Control control)
+        {
+            // Проверяем, что элемент не первый кнопка и еще не в самом нижнем положении формы
+            if (control != button1 && control.Top + control.Height < this.ClientSize.Height)
+            {
+                int newTop = Math.Min(this.ClientSize.Height - control.Height, control.Top + 10); // Увеличиваем значение Top на 10 пикселей, но не выше высоты формы
+                control.Top = newTop;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Toggle the movement mode when the first button is clicked
+            ModeButton_Click(sender, e);
+        }
+
+    }
+}
